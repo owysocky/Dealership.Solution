89fe3b98385352899c992c4e869cc80c6285a523
Olga Wysocky and Pavel Zanchuk <olga@>
2019-02-19 13:16:00 -0800

    add controllers and tests

--- Dealership.Test/ControllerTests/HomeControllerTests.cs
+++ Dealership.Test/ControllerTests/HomeControllerTests.cs
@@ -1,14 +1,65 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using ToDoList.Controllers;
-using ToDoList.Models;
+using Dealership.Controllers;
+using Dealership.Models;
 
 namespace Dealership.Tests
 {
-    [TestClass]
-    public class HomeControllerTest
+  [TestClass]
+  public class HomeControllerTest
+  {
+    [TestMethod]
+    public void Index_ReturnsCorrectView_True()
     {
+      //Arrange
+      HomeController controller = new HomeController();
 
+      //Act
+      ActionResult indexView = controller.Index();
+
+      //Assert
+      Assert.IsInstanceOfType(indexView, typeof(ViewResult));
     }
+
+    [TestMethod]
+    public void Index_HasCorrectModelType_CarList()
+    {
+      //Arrange
+      ViewResult indexView = new HomeController().Index() as ViewResult;
+
+      //Act
+      var result = indexView.ViewData.Model;
+
+      //Assert
+      Assert.IsInstanceOfType(result, typeof(List<Car>));
+    }
+
+    [TestMethod]
+    public void Create_ReturnsCorrectActionType_RedirectToActionResult()
+    {
+      //Arrange
+      CarsController controller = new CarsController();
+
+      //Act
+      IActionResult view = controller.Create("2014", "MakeModel", "22000", "2000");
+
+      //Assert
+      Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
+    }
+
+    [TestMethod]
+    public void Create_RedirectsToCorrectAction_Index()
+    {
+      //Arrange
+      CarsController controller = new CarsController();
+      RedirectToActionResult actionResult = controller.Create("2014", "MakeModel", "22000", "2000") as RedirectToActionResult;
+
+      //Act
+      string result = actionResult.ActionName;
+
+      //Assert
+      Assert.AreEqual(result, "Index");
+    }
+  }
 }
--- Dealership/Controllers/HomeController.cs
+++ Dealership/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Dealership.Models;
-using System.Collections.Generic;
 
 namespace Dealership.Controllers
 {
@@ -10,21 +8,7 @@ namespace Dealership.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      List<Car> allCars = Car.GetAll();
-      return View(allCars);
-    }
-
-    [HttpGet("/cars/new")]
-    public ActionResult CreateForm()
-    {
       return View();
-    }
-
-    [HttpPost("/cars")]
-    public ActionResult Create(string year, string makeModel, string miles, string price)
-    {
-      Car myCar = new Car(year, makeModel, miles, price);
-      return RedirectToAction("Index");
     }
 
   }
